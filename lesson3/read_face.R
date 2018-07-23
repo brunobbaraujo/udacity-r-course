670b285c2ccab0e71ecae357f32cd1a5c9a405cd
Bruno Bento <brunobbdearaujo@gmail.com>
2018-07-23 16:57:09 -0300

    Solve until 10th exercise

--- lesson3/read_face.R
+++ lesson3/read_face.R
@@ -7,4 +7,4 @@ ggplot(data = pf, aes(x = dob_day)) +
   scale_x_continuous(breaks = 1:31) + 
   facet_wrap(~dob_month)
 
-qplot(x=friend_count,data = pf, xlim = c(0, 1000))
+qplot(x=friend_count,data = pf,binwidth=25) + scale_x_continuous(limits = c(0,1000),breaks=seq(0,1000,50))+facet_wrap(~gender)
