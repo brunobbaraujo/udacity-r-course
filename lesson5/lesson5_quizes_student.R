5bbaf67a2c875b77e9f16af3de6bfbb9f4843ab0
Bruno Bento <brunobbdearaujo@gmail.com>
2018-09-11 01:31:22 -0300

    resolve até exercicio 5 do modulo 8 de R

--- lesson5/lesson5_quizes_student.R
+++ lesson5/lesson5_quizes_student.R
@@ -8,9 +8,39 @@ pf <- diamonds
 rm(diamonds)
 
 
-### 1. Histogram Price prices facet by color
+# 1. Histogram Price prices facet by color
 ggplot(aes(x = price), data = pf) +
-  geom_histogram(aes(color = cut),binwidth = 100) +
-  scale_fill_brewer(type = 'qual') +
+  geom_histogram(aes(color = cut)) +
+  scale_x_log10() +
   facet_wrap(~ color) +
-  coord_cartesian(xlim=c(0,12000))
+  scale_fill_brewer(type = 'qual')
+  
+# 2. Scatterplot of diamond price vs. 
+# table and color the points by the cut
+ggplot(aes(x = table, y = price), data = pf) +
+  geom_point(aes(color = cut),size = 5) +
+  scale_color_brewer(type = 'qual') +
+  coord_cartesian(xlim=c(50,80))
+
+# 4. scatterplot of diamond price vs.
+# volume (x * y * z) and color the points by
+# the clarity of diamonds. Use scale on the y-axis
+# to take the log10 of price. You should also
+# omit the top 1% of diamond volumes from the plot.
+  
+pf$volume <- with(pf, x*y*z)
+ggplot(aes(x = volume, y = price), data = pf) +
+  geom_point(aes(color = clarity),size = 3) +
+  scale_y_log10() +
+  scale_color_brewer(type = 'div') +
+  coord_cartesian(xlim=c(0,quantile(pf$volume,0.99)))
+  
+# 5. Create a line graph of the median proportion of
+# friendships initiated ('prop_initiated') vs.
+# tenure and color the line segment by
+# year_joined.bucket.
+pf$year_joined <- floor(2014 - pf$tenure/365)
+pf$year_joined.bucket <- cut(pf$year_joined, breaks = c(2004,2009, 2011, 2012,2014), right = TRUE)
+  
+
+  
